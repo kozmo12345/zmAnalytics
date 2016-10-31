aada233b9acbbf720960d27828ed8e74bbb67e59
kozmo12345@nate.com <kozmo12345@nate.com>
2016-10-31 17:42:16 +0900

    add mesu

--- set.py
+++ set.py
@@ -7,9 +7,13 @@ import matplotlib.pyplot as plt
 import datetime
 import time
 
-sp.random.seed(3)  # 이후에 같은 데이터를 생성하기 위해
+sp.random.seed(3)
 
-today = datetime.datetime.now().strftime('%Y-%m-%d')
+now = datetime.datetime.now()
+today = now.strftime('%Y-%m-%d')
+hour = now.hour
+minute = now.minute
+second = now.second - 1
 
 setFile = open(os.path.join("C:\\", "Dropbox\\Data\\" + today + "\\" + today + "m.txt"), 'w')
 realfilePath = os.path.join("C:\\", "Dropbox\\Data\\" + today + "\\" + today + ".txt");
@@ -18,11 +22,10 @@ data = sp.genfromtxt(realfilePath, delimiter="\t", dtype='|S20')
 codes = sp.unique(data[data[:,7] != b''][:,7])
 times = sp.unique(data[data[:,0] != b''][:,0])
 
-second_oTime = datetime.timedelta(hours=9,minutes=2,seconds=3).total_seconds() #1차계산시간
+second_oTime = datetime.timedelta(hours=hour,minutes=minute,seconds=second).total_seconds() #계산시간
 str_oTime = ""
 bool_oTime = False
 
-print(1)
 for i, t in enumerate(times):
     x = time.strptime(t.decode('utf-8'), '%H:%M:%S')
     nt = datetime.timedelta(hours=x.tm_hour,minutes=x.tm_min,seconds=x.tm_sec).total_seconds()
@@ -32,20 +35,23 @@ for i, t in enumerate(times):
         bool_oTime = True
         break;
 
-print(2)
+if(second_oTime < 32500):
+    bool_oTime = False
 if(bool_oTime == True):
-    
     for ci, code in enumerate(codes):
         exportData = data[data[:,7] == code]
         
-        x = time.strptime(exportData[0,0].decode('utf-8'), '%H:%M:%S')
-        firstSecond = datetime.timedelta(hours=x.tm_hour,minutes=x.tm_min,seconds=x.tm_sec).total_seconds()
+        xtime = time.strptime(exportData[0,0].decode('utf-8'), '%H:%M:%S')
+        firstSecond = datetime.timedelta(hours=xtime.tm_hour,minutes=xtime.tm_min,seconds=xtime.tm_sec).total_seconds()
     
+        ti = sp.array([])
         c = exportData[:, 3].astype(float)
         i = -1
         for ei, et in enumerate(exportData[:, 0]):
             tsi = time.strptime(et.decode('utf-8'), '%H:%M:%S')
             sect = datetime.timedelta(hours=tsi.tm_hour,minutes=tsi.tm_min,seconds=tsi.tm_sec).total_seconds()
+            v_time = sect - firstSecond
+            ti = sp.append(ti, sp.sqrt(v_time)/2)
             if(second_oTime == sect):
                 i = ei
                 break;
@@ -55,7 +61,25 @@ if(bool_oTime == True):
         rate = exportData[i, 3].decode('UTF-8')
         grade = int(exportData[i, 1].decode('UTF-8'))
         gr = int(exportData[i, 4].decode('UTF-8'))
+        
+        if(grade < 30 and gr > 460000):
+            ms_md = (exportData[i,5].astype(float))/(exportData[i,6].astype(float))
+            sms_md = sp.sum((sp.sum(exportData[:i+1,5].astype(float)))/(sp.sum(exportData[:i+1,6].astype(float))))
+        
+            if(ms_md > 1 and sms_md > 1):
+                x = ti
+                y = exportData[:i+1,3].astype(float)
+                if(len(y) <= 1):
+                    break
+                level = 1
+                fit = sp.polyfit(x, y, level)
+                gradient = sp.around(fit[0]*10, decimals=2)
 
-        if(grade < 30 and gr > 1000000):
-            setFile.write( str(code.decode('utf-8')) + ',' + str(float(rate)) + '\n')
-print(3)
+                maxr = 100000
+                ry = (exportData[:i+1,4].astype(float))/maxr
+                srlist = [b - a for a,b in zip(ry,ry[1:])]
+                srfit = sp.polyfit(x[:-1], srlist, level)
+                srgrad = sp.around(srfit[0]*10, decimals=2)
+                
+                if(gradient >= 0.7 and srgrad > -0.01):
+                    setFile.write( str(code.decode('utf-8')) + ',' + str(float(rate)) + ',' + str(gradient) +  ',' + str_oTime + '\n')
