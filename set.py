94fb470a2d8eaec4e1f1db25f651008dcd5ca109
kozmo12345@nate.com <kozmo12345@nate.com>
2016-11-02 11:33:44 +0900

    a

--- set.py
+++ set.py
@@ -21,11 +21,9 @@ def readData(filePath):
 
     return fdata
 
-setFile = open(os.path.join("C:\\", "Dropbox\\Data\\" + today + "\\" + today + "m.txt"), 'w')
-    
 today = datetime.datetime.now().strftime('%Y-%m-%d')
 startTime = datetime.timedelta(hours=9,minutes=00,seconds=10).total_seconds()
-endTime = datetime.timedelta(hours=12,minutes=00,seconds=30).total_seconds()
+endTime = datetime.timedelta(hours=10,minutes=00,seconds=30).total_seconds()
 temp_sec = 0
 while(True):
     now = datetime.datetime.now()
