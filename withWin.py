2f6ca5b3d8b1c3ecf25eebbbb32e06bc55631eac
kozmo12345@nate.com <kozmo12345@nate.com>
2016-12-12 09:38:36 +0900

    jdisa

--- withWin.py
+++ withWin.py
@@ -125,7 +125,6 @@ while(True):
             xstime = time.strptime(ttime.decode('utf-8'), '%H:%M:%S')
             second_oTime = datetime.timedelta(hours=xstime.tm_hour,minutes=xstime.tm_min,seconds=xstime.tm_sec).total_seconds() #계산시간
             str_oTime = ttime.decode('utf-8')
-            bool_oTime = False
 
             if(second_oTime > endTime and second_oTime < nowTime - 120 ):
                 continue;            
@@ -138,94 +137,93 @@ while(True):
 
             tmp_time = second_oTime
             
-            if(bool_oTime == True):
-                ttimeData = data[data[:,0] == ttime]
-                ttimeData2 = ttimeData[ttimeData[:,1].astype(int) < 21]
-                ttimeData3 = ttimeData2[ttimeData2[:,4].astype(int) > 455000]
-                ttimeData4 = ttimeData3[ttimeData3[:,3].astype(float) < 25]
-                ttimeData5 = ttimeData4[ttimeData4[:,8].astype(float) > 2000]
-                codes = ttimeData5[:,7]
-                if(second_oTime > endTime and len(comps) > 0):
-                    codes = comps;
-                for code in (codes):
-                    if(code == b''):
-                        continue;
-                    exportData = data[data[:,7] == code]
+            ttimeData = data[data[:,0] == ttime]
+            ttimeData2 = ttimeData[ttimeData[:,1].astype(int) < 21]
+            ttimeData3 = ttimeData2[ttimeData2[:,4].astype(int) > 455000]
+            ttimeData4 = ttimeData3[ttimeData3[:,3].astype(float) < 25]
+            ttimeData5 = ttimeData4[ttimeData4[:,8].astype(float) > 2000]
+            codes = ttimeData5[:,7]
+            if(second_oTime > endTime and len(comps) > 0):
+                codes = comps;
+            for code in (codes):
+                if(code == b''):
+                    continue;
+                exportData = data[data[:,7] == code]
+                
+                xtime = time.strptime(exportData[0,0].decode('utf-8'), '%H:%M:%S')
+                firstSecond = datetime.timedelta(hours=xtime.tm_hour,minutes=xtime.tm_min,seconds=xtime.tm_sec).total_seconds()
+                
+                ti = sp.array([])
+
+                i = -1
+                for ei, et in enumerate(exportData[:, 0]):
+                    tsi = time.strptime(et.decode('utf-8'), '%H:%M:%S')
+                    sect = datetime.timedelta(hours=tsi.tm_hour,minutes=tsi.tm_min,seconds=tsi.tm_sec).total_seconds()
+                    v_time = sect - firstSecond
+                    ti = sp.append(ti, (v_time)/10)
+                    if(second_oTime == sect):
+                        i = ei
+                        break;
                     
-                    xtime = time.strptime(exportData[0,0].decode('utf-8'), '%H:%M:%S')
-                    firstSecond = datetime.timedelta(hours=xtime.tm_hour,minutes=xtime.tm_min,seconds=xtime.tm_sec).total_seconds()
-                    
-                    ti = sp.array([])
+                if(i == -1): continue
 
-                    i = -1
-                    for ei, et in enumerate(exportData[:, 0]):
-                        tsi = time.strptime(et.decode('utf-8'), '%H:%M:%S')
-                        sect = datetime.timedelta(hours=tsi.tm_hour,minutes=tsi.tm_min,seconds=tsi.tm_sec).total_seconds()
-                        v_time = sect - firstSecond
-                        ti = sp.append(ti, (v_time)/10)
-                        if(second_oTime == sect):
-                            i = ei
-                            break;
-                        
-                    if(i == -1): continue
+                if(code in comps and code not in medos and code.decode('utf-8') in mesuStart and second_oTime > mesuStart[code.decode('utf-8')]):
+                    mmRate = (sp.sum(exportData[i-2:i+1,5].astype(float)))/(sp.sum(exportData[i-2:i+1,6].astype(float)))
+                    ms = float(msRate[code.decode('utf-8')])
+                    md = float(exportData[i, 3].decode('UTF-8'))
+                    ed = round(md - ms, 2)
+                    print(code.decode('utf-8') + '    ' + str(mmRate) + '    ' + str(str_oTime))
+                    if(mmRate < rateLimit and ed >= wanna):
+                        mdFile = open(mdFilePath, 'a')
+                        mdFile.write(str(code.decode('utf-8')) + ',' + str(float(exportData[i + 1, 3].decode('UTF-8'))) + ',' + str(exportData[i, 0].decode('UTF-8')) + ',' + str(datetime.datetime.now().strftime('%H:%M:%S')) + '\n')
+                        mdFile.close()
+                        medos.append(code)
+                        comps.remove(code)
 
-                    if(code in comps and code not in medos and code.decode('utf-8') in mesuStart and second_oTime > mesuStart[code.decode('utf-8')]):
-                        mmRate = (sp.sum(exportData[i-2:i+1,5].astype(float)))/(sp.sum(exportData[i-2:i+1,6].astype(float)))
-                        ms = float(msRate[code.decode('utf-8')])
-                        md = float(exportData[i, 3].decode('UTF-8'))
-                        ed = round(md - ms, 2)
-                        print(code.decode('utf-8') + '    ' + str(mmRate) + '    ' + str(str_oTime))
-                        if(mmRate < rateLimit and ed >= wanna):
-                            mdFile = open(mdFilePath, 'a')
-                            mdFile.write(str(code.decode('utf-8')) + ',' + str(float(exportData[i + 1, 3].decode('UTF-8'))) + ',' + str(exportData[i, 0].decode('UTF-8')) + ',' + str(datetime.datetime.now().strftime('%H:%M:%S')) + '\n')
-                            mdFile.close()
-                            medos.append(code)
-                            comps.remove(code)
+                if(second_oTime > endTime):
+                    continue;
 
-                    if(second_oTime > endTime):
-                        continue;
-
-                    if(nowTime > endTime):
-                        continue;
+                if(nowTime > endTime):
+                    continue;
     
-                    rate = exportData[i, 3].decode('UTF-8')
-                    grade = int(exportData[i, 1].decode('UTF-8'))
-                    gr = int(exportData[i, 4].decode('UTF-8'))
+                rate = exportData[i, 3].decode('UTF-8')
+                grade = int(exportData[i, 1].decode('UTF-8'))
+                gr = int(exportData[i, 4].decode('UTF-8'))
 
-                    ms_md = (exportData[i,5].astype(float))/(exportData[i,6].astype(float))
-                    sms_md = sp.sum((sp.sum(exportData[:i+1,5].astype(float)))/(sp.sum(exportData[:i+1,6].astype(float))))
+                ms_md = (exportData[i,5].astype(float))/(exportData[i,6].astype(float))
+                sms_md = sp.sum((sp.sum(exportData[:i+1,5].astype(float)))/(sp.sum(exportData[:i+1,6].astype(float))))
 
-                    if(ms_md > 0.96 and sms_md > 0.97 and grade < 13):
-                        x = ti
-                        y = exportData[:i+1,3].astype(float)
-                        if(len(y) <= 1):
-                            break
-                        level = 1
-                        fit = sp.polyfit(x, y, level)
-                        gradient = sp.around(fit[0]*10, decimals=2)
+                if(ms_md > 0.96 and sms_md > 0.97 and grade < 13):
+                    x = ti
+                    y = exportData[:i+1,3].astype(float)
+                    if(len(y) <= 1):
+                        break
+                    level = 1
+                    fit = sp.polyfit(x, y, level)
+                    gradient = sp.around(fit[0]*10, decimals=2)
             
-                        maxr = 100000
-                        ry = (exportData[:i+1,4].astype(float))/maxr
-                        srlist = [b - a for a,b in zip(ry,ry[1:])]
-                        srfit = sp.polyfit(x[:-1], srlist, level)
-                        srgrad = sp.around(srfit[0]*10, decimals=2)
-                        
-                        if(gradient >= 0.4 and srgrad > 0):
+                    maxr = 100000
+                    ry = (exportData[:i+1,4].astype(float))/maxr
+                    srlist = [b - a for a,b in zip(ry,ry[1:])]
+                    srfit = sp.polyfit(x[:-1], srlist, level)
+                    srgrad = sp.around(srfit[0]*10, decimals=2)
+                    
+                    if(gradient >= 0.4 and srgrad > 0):
            
-                            if(code.decode('utf-8') in mesuDict):
-                                mesuDict[code.decode('utf-8')] = mesuDict[code.decode('utf-8')] + 1
-                            else:
-                                mesuDict[code.decode('utf-8')] = mesuDict.get(code.decode('utf-8'), 0)
-                            
-                            if(mesuDict[code.decode('utf-8')] == mesuLimit and (code not in comps) and (code not in medos)):
-                                print(str(ttime) + " " + str(gradient) + " " + str(srgrad) + " " + str(grade))
-                                comps.append(code)
-                                mesuStart[code.decode('utf-8')] = second_oTime
-                                msRate[code.decode('utf-8')] = float(rate)
-                                cost = exportData[i, 8].decode('UTF-8')
-                                setFile = open(setFilePath, 'a')
-                                setFile.write( str(code.decode('utf-8')) + ',' + str(float(rate)) + ',' + str(gradient) +  ',' + str_oTime + ',' + str(wanna) + ',' + str(datetime.datetime.now().strftime('%H:%M:%S')) + ',' + str(cost) + '\n')
-                                setFile.close()
+                        if(code.decode('utf-8') in mesuDict):
+                            mesuDict[code.decode('utf-8')] = mesuDict[code.decode('utf-8')] + 1
+                        else:
+                            mesuDict[code.decode('utf-8')] = mesuDict.get(code.decode('utf-8'), 0)
+                        
+                        if(mesuDict[code.decode('utf-8')] == mesuLimit and (code not in comps) and (code not in medos)):
+                            print(str(ttime) + " " + str(gradient) + " " + str(srgrad) + " " + str(grade))
+                            comps.append(code)
+                            mesuStart[code.decode('utf-8')] = second_oTime
+                            msRate[code.decode('utf-8')] = float(rate)
+                            cost = exportData[i, 8].decode('UTF-8')
+                            setFile = open(setFilePath, 'a')
+                            setFile.write( str(code.decode('utf-8')) + ',' + str(float(rate)) + ',' + str(gradient) +  ',' + str_oTime + ',' + str(wanna) + ',' + str(datetime.datetime.now().strftime('%H:%M:%S')) + ',' + str(cost) + '\n')
+                            setFile.close()
                              
         except Exception as e:
             print('--------------------' + str(e))
