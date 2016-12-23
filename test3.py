887e8184e6d8937150594820a5d52aefab012a7f
kozmo12345@nate.com <kozmo12345@nate.com>
2016-12-23 12:38:13 +0900

    aksdo

--- test3.py
+++ test3.py
@@ -9,19 +9,22 @@ import time
 
 sp.random.seed(3)  # 이후에 같은 데이터를 생성하기 위해
 
-def plot_models(x, cs, models, mstime, fname=None, mx=None, ymax=None, xmin=None):
+def plot_models(x, cs, gy, mstime, fname=None, mesuIndex=None, mesuValue=None, ymax=None, xmin=None):
     colors = ['g', 'k', 'b', 'm', 'r']
     linestyles = ['-', '-.', '--', ':', '-']
 
     plt.clf()
+    plt.plot(mesuIndex, mesuValue, "o", color='red')
     plt.scatter(x, cs, s=20)
+    # positions = range(1, len(gy) + 1)
+    plt.bar(x[:-1], gy, align='center')
     plt.title("graph")
     plt.xlabel("Time")
     plt.ylabel("Rate")
-    # mx = sp.linspace(0, x[-1], 1000)
+    mx = sp.linspace(0, x[-1], 1000)
     # plt.plot([mstime]*len(mx), mx, linestyle=':', linewidth=2, c='m')
 
-    plt.legend(["d=%i" % m.order for m in models], loc="upper right")
+    # plt.legend(["d=%i" % m.order for m in models], loc="upper right")
     
     plt.autoscale(tight=True)
     plt.ylim(ymin=0)
@@ -42,20 +45,24 @@ dates = []
 bcodes = []
 mesutimes = []
 medotimes = []
+geteds = []
 
 edFilePath = os.path.join("C:\\", "Dropbox\\Data\\" + "ed.txt");
 edFile = open(edFilePath, 'r')
 
 for line in edFile:
     eds = line.split(',')
+    if(eds[0] == 'day'):
+        continue
     dates.append(eds[0])
     bcodes.append(eds[3])
     mesutimes.append(eds[10])
     medotimes.append(eds[11])
+    geteds.append(eds[9])
 print("end")            
 
 for datei, da in enumerate(dates):
-    
+
     date = da
     setcode = bcodes[datei] #b'038950'
     
@@ -89,7 +96,10 @@ for datei, da in enumerate(dates):
             medoIndex = i
             break;
 
-    c = exportData[mesuIndex:medoIndex, 3].astype(float)
+    c = exportData[0:medoIndex, 3].astype(float)
+    g = exportData[0:medoIndex, 4].astype(float)
+    gy = [(b - a) for a,b in zip(g,g[1:])]
+    gy = [x/(max(gy))*10 for x in gy]
 
     img_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "grd_img")
-    plot_models([x * 10 for x in range(len(c))], c, [], mstime = "" ,fname = os.path.join(img_dir, str(dates[datei])+ '_' +str(bcodes[datei]) + ".png"))
+    plot_models([x for x in range(len(c))], c, gy, mstime = "" ,fname = os.path.join(img_dir, str(dates[datei])+ '_' +str(bcodes[datei]) + '_' +str(geteds[datei]) + ".png"), mesuIndex = [mesuIndex], mesuValue = [exportData[mesuIndex, 3].astype(float)])
